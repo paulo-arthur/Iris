4117fcd4af44ad86adff1e33ccf63fad7cdc4649
Arthur Palhano <pauloart.hur670@gmail.com>
2020-12-13 21:57:55 -0300

    adicionando o codigo em python

---
+++ imgDataDecode.py
@@ -0,0 +1,42 @@
+from PIL import Image
+import math
+
+chars = ['', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', '.', ',', '>', '<', '?', ';', ':', "'", '[', ']', '{', '}', '-', '_', '=', '+', '(', ')', '*', '&', '^', '%', '$', '#', '@', '!', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'á', 'é', 'í', 'ó', 'à', 'ã', 'õ', 'ç', 'Á', 'É', 'Í', 'Ó', 'À', 'Ã', 'Õ', 'Ç', 'ê', 'Ê', 'â', 'Â', 'ô', 'Ô', 'ú', 'Ú']    
+
+img = Image.open(str(input("Nome do arquivo: ")) + ".png")
+img.convert("RGB")
+pix = img.load()
+
+pixelsStorage = []
+
+for y in range(0, img.size[1] - 0):
+    for x in range(0, img.size[0] - 0):
+        pixelsStorage.append(pix[x, y])
+
+msgLen = len(pixelsStorage) * 3
+msgPixLen = math.ceil(msgLen/3)
+msgChars = ()
+
+for pixel in range(0, msgPixLen):
+    if pixelsStorage[pixel:pixel+5] == [(0, 0, 0)]*5:
+        break
+    else:
+        if math.floor(33.333333333*len(msgChars)/len(pixelsStorage)) % 5 == 0:
+            print(math.floor(33.333333333*len(msgChars)/len(pixelsStorage)), "%")
+        msgChars += pixelsStorage[pixel]
+
+msgChars = list(msgChars)
+msgChars = msgChars[:(len(msgChars) - (msgPixLen * 3 - msgLen))]
+
+msg = ""
+
+for char in msgChars:
+    if char > len(chars)-1:
+        char = len(chars)-1
+    msg += chars[char]
+
+if len(pixelsStorage) != img.size[0] * img.size[1]:
+    print("Caracteres podem ter sido perdidos")
+
+print("Mensagem oculta: ")
+print(msg)
---
+++ imgDataEncode.py
@@ -0,0 +1,27 @@
+from PIL import Image
+import math
+import random
+
+chars = ['', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', '.', ',', '>', '<', '?', ';', ':', "'", '[', ']', '{', '}', '-', '_', '=', '+', '(', ')', '*', '&', '^', '%', '$', '#', '@', '!', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'á', 'é', 'í', 'ó', 'à', 'ã', 'õ', 'ç', 'Á', 'É', 'Í', 'Ó', 'À', 'Ã', 'Õ', 'Ç', 'ê', 'Ê', 'â', 'Â', 'ô', 'Ô', 'ú', 'Ú']
+
+msg = str(input("Digite a mensagem para codificar: "))
+msgLen = len(msg)
+
+msgPixels = []
+
+for char in msg:
+    msgPixels.append(chars.index(char))
+
+print(msgPixels)
+
+while len(msgPixels) % 3 != 0:
+    msgPixels.append(0)
+
+imgData = []
+
+for pixel in range(0, len(msgPixels), 3):
+    imgData.append(tuple([msgPixels[pixel], msgPixels[pixel+1], msgPixels[pixel+2]]))
+
+newImg = Image.new("RGB", (480, 480))
+newImg.putdata(imgData)
+newImg.save("pos.png")
